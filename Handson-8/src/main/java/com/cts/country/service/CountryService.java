4ea56f84c879f091915c63c9c906cd27fccf9eff
Bavani.S <mail2bavani.s@gmail.com>
2021-06-12 20:37:51 +0530

    Committed

---
+++ Handson-8/src/main/java/com/cts/country/service/CountryService.java
@@ -0,0 +1,47 @@
+package com.cts.country.service;
+
+
+import java.util.Optional;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cts.country.model.Country;
+import com.cts.country.repository.CountryRepository;
+import com.cts.country.service.exception.CountryNotFoundException;
+
+@Service
+public class CountryService {
+
+	@Autowired
+	CountryRepository countryRepository;
+	
+	@Transactional
+	public void addCountry(Country country)
+	{
+		countryRepository.save(country);
+		System.out.println("Country detail saved successfully!!!");
+	}
+	@Transactional
+	public Country findCountryByCode(String countryCode) throws CountryNotFoundException{
+		Optional<Country> result = countryRepository.findById(countryCode);
+		if (!result.isPresent())
+			throw new CountryNotFoundException();
+		Country country = result.get();
+		return country;
+	}
+	@Transactional
+	public void updateCountry(String code, String name) {
+		Country country;
+		try {
+			country = findCountryByCode(code);
+			country.setName(name);
+			addCountry(country);
+		} catch (CountryNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
---
+++ Handson-8/src/main/java/com/cts/country/service/exception/CountryNotFoundException.java
@@ -0,0 +1,9 @@
+package com.cts.country.service.exception;
+
+@SuppressWarnings("serial")
+public class CountryNotFoundException extends Exception {
+public CountryNotFoundException() {
+	// TODO Auto-generated constructor stub
+	super();
+}
+}
