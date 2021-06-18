40398f72cb9243787c5dfa9f3bf1a7c3c68b9905
Bavani.S <mail2bavani.s@gmail.com>
2021-06-18 22:07:13 +0530

    Committed

--- FlightManagement/src/test/java/com/cts/FlightManagementApplicationTests.java
+++ FlightManagement/src/test/java/com/cts/FlightManagementApplicationTests.java
@@ -2,105 +2,120 @@ package com.cts;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.cts.model.Flight;
 import com.cts.model.Passenger;
+import com.cts.service.FlightService;
 
 @SpringBootTest
 class FlightManagementApplicationTests {
+	FlightService flightService;
 
-/*	@Test
+	@Test
 	void addEconomyFlight() {
-		Flight flight = new Flight(1,"economy",null);
-		assertEquals("economy",flight.getFlightType());
+		Flight flight = new Flight();
+		flight.setId(1);
+		flight.setFlightType("economy");
+		flightService.addFlight(flight);
+		flight=flightService.getFlight(1);
+		assertNotNull(flight);
 	}
 	@Test
 	void addBusinessFlight() {
-		Flight flight = new Flight(1,"business",null);
-		assertEquals("business",flight.getFlightType());
+		Flight flight = new Flight();
+		flight.setId(2);
+		flight.setFlightType("business");
+		flightService.addFlight(flight);
+		flight=flightService.getFlight(2);
+		assertNotNull(flight);
 	}
 	@Test
 	void addVipToEconomy() {
 		Flight flight = new Flight();
-		flight.setId(3);
-		flight.setFlightType("economy");
-		Passenger passenger = new Passenger("Sam", true);
-		flight.addPassenger(passenger);
-		assertNotEquals(1, flight.getPassengers().size());
+		flight=flightService.getFlight(1);
+		Passenger passenger=new Passenger();
+		passenger.setName("sam");
+		passenger.setVip(true);
+		flightService.addPassenger(flight, passenger);
+		assertNotNull(flightService.getFlight(flight.getId()));
 	}
 	
 	@Test
 	void addVipToBusiness() {
 		Flight flight = new Flight();
-		flight.setId(4);
-		flight.setFlightType("business");
-		Passenger passenger = new Passenger("Dean", true);
-		flight.addPassenger(passenger);
-		assertEquals(1, flight.getPassengers().size());
+		flight=flightService.getFlight(2);
+		Passenger passenger=new Passenger();
+		passenger.setName("dean");
+		passenger.setVip(true);
+		flightService.addPassenger(flight, passenger);
+		assertNotNull(flightService.getFlight(flight.getId()));
 	}
 	
 	@Test
 	void addNonVipToEconomy() {
 		Flight flight = new Flight();
-		flight.setId(5);
-		flight.setFlightType("economy");
-		Passenger passenger = new Passenger("Jacob", false);
-		flight.addPassenger(passenger);
-		assertEquals(1, flight.getPassengers().size());
-	}
+		flight=flightService.getFlight(1);
+		Passenger passenger=new Passenger();
+		passenger.setName("bobby");
+		passenger.setVip(false);
+		flightService.addPassenger(flight, passenger);
+		assertNotNull(flightService.getFlight(flight.getId()));
+		}
 	
 	@Test
 	void addNonVipToBusiness() {
 		Flight flight = new Flight();
-		flight.setId(5);
-		flight.setFlightType("business");
-		Passenger passenger = new Passenger("Jacob", false);
-		flight.addPassenger(passenger);
-		assertEquals(0, flight.getPassengers().size());
+		flight=flightService.getFlight(2);
+		Passenger passenger=new Passenger();
+		passenger.setName("adam");
+		passenger.setVip(false);
+		flightService.addPassenger(flight, passenger);
+		assertNull(flightService.getFlight(flight.getId()));
 	}
 	@Test
 	void RemoveVIPFromEconomy() {
 		Flight flight = new Flight();
-		flight.setId(7);
-		flight.setFlightType("economy");
-		Passenger passenger = new Passenger("Dean", true);
-		flight.addPassenger(passenger);
-		flight.removePassenger(passenger);
-		assertEquals(0, flight.getPassengers().size());
+		flight=flightService.getFlight(1);
+		Passenger passenger=new Passenger();
+		passenger.setName("sam");
+		passenger.setVip(true);
+		flightService.removePassenger(flight, passenger);
+		assertNull(flightService.getFlight(flight.getId()));
 	}
 	@Test
 	void RemoveVIPFromBusiness() {
 		Flight flight = new Flight();
-		flight.setId(7);
-		flight.setFlightType("business");
-		Passenger passenger = new Passenger("Dean", true);
-		flight.addPassenger(passenger);
-		flight.removePassenger(passenger);
-		assertEquals(1, flight.getPassengers().size());
-	}
+		flight=flightService.getFlight(2);
+		Passenger passenger=new Passenger();
+		passenger.setName("dean");
+		passenger.setVip(true);
+		flightService.removePassenger(flight, passenger);
+		assertNotNull(flightService.getFlight(flight.getId()));	}
 	@Test
 	void RemoveNonVIPFromEconomy() {
 		Flight flight = new Flight();
-		flight.setId(7);
-		flight.setFlightType("economy");
-		Passenger passenger = new Passenger("Dean", false);
-		flight.addPassenger(passenger);
-		flight.removePassenger(passenger);
-		assertEquals(0, flight.getPassengers().size());
+		flight=flightService.getFlight(1);
+		Passenger passenger=new Passenger();
+		passenger.setName("bobby");
+		passenger.setVip(false);
+		flightService.removePassenger(flight, passenger);
+		assertNull(flightService.getFlight(flight.getId()));
 		
 	}
 	@Test
 	void RemoveNonVIPFromBusiness() {
 		Flight flight = new Flight();
-		flight.setId(7);
-		flight.setFlightType("business");
-		Passenger passenger = new Passenger("Dean", false);
-		flight.addPassenger(passenger);
-		flight.removePassenger(passenger);
-		assertEquals(0, flight.getPassengers().size());
+		flight=flightService.getFlight(2);
+		Passenger passenger=new Passenger();
+		passenger.setName("adam");
+		passenger.setVip(false);
+		flightService.removePassenger(flight, passenger);
+		assertNull(flightService.getFlight(flight.getId()));
 	}
-*/
+
 }
